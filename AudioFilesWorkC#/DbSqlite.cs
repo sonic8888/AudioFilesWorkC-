c0529c8542d8d11270b8965afabf0b40b1e8fd82
sonic <sonic45600@gmail.com>
2025-07-29 19:20:20 +0300

    add DbSqlite

---
+++ AudioFilesWorkC#/DbSqlite.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioFilesWorkC_
+{
+    internal static class DbSqlite
+    {
+        public static string Get_str_connection(string? data_sours, string mode = "ReadOnly", Dictionary<string, string>? paramss = null)
+        {
+            string str = $"Data Source={data_sours};Mode={mode};";
+            if (paramss != null)
+            {
+                foreach (var item in paramss)
+                {
+                    string param = $"{item.Key}={item.Value};";
+                    str += item;
+                }
+            }
+            return str;
+        }
+
+        public static int ExecuteScalar(string str_connection, string sqlExpression, List<SqliteParameter>? sql_params=null)
+        {
+            object? val = 0;
+            int res = 0;
+            using (var connection = new SqliteConnection(str_connection))
+            {
+                connection.Open();
+                SqliteCommand command = new SqliteCommand(sqlExpression, connection);
+                if (sql_params != null)
+                {
+                    foreach (var item in sql_params)
+                    {
+                        command.Parameters.Add(item);
+                    }
+                }
+                    val = command.ExecuteScalar();
+            }
+            res = Convert.ToInt32(val);
+            return res;
+        }
+
+        public static SqliteParameter Get_sql_parametr(string name, int value)
+        {
+            SqliteParameter par = new SqliteParameter(name, value);
+            return par;
+
+        }
+
+        public static SqliteParameter Get_sql_parametr(string name, string value)
+        {
+            SqliteParameter par = new SqliteParameter(name, value);
+            return par;
+
+        }
+    }
+}
--- AudioFilesWorkC#/Program.cs
+++ AudioFilesWorkC#/Program.cs
@@ -1,10 +1,13 @@
-﻿using System.Runtime.CompilerServices;
+﻿using Microsoft.Data.Sqlite;
+using System.Runtime.CompilerServices;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace AudioFilesWorkC_
 {
     internal class Program
     {
+
+        static string str = "SELECT Count(TrackId) FROM T_PlaylistTrack WHERE Kind = @value;";
         static void Main(string[] args)
         {
 
@@ -13,7 +16,16 @@ namespace AudioFilesWorkC_
                 YandexMusic yandexMusic = new YandexMusic();
                 Console.WriteLine(yandexMusic.PathYndexMusicDir);
                 Console.WriteLine(yandexMusic.PathMusicSours);
-                Console.WriteLine(yandexMusic.PathDBSqlite);
+                //Console.WriteLine(yandexMusic.PathDBSqlite);
+                string? sours_db = yandexMusic.PathDBSqlite;
+                string sql_conn = DbSqlite.Get_str_connection(sours_db);
+                Console.WriteLine(sql_conn);
+                List<SqliteParameter> com_params = new List<SqliteParameter>();
+                //com_params.Add(DbSqlite.Get_sql_parametr("$field", "TrackId"));
+                //com_params.Add(DbSqlite.Get_sql_parametr("$table", "T_PlaylistTrack"));
+                com_params.Add(DbSqlite.Get_sql_parametr("value", "5"));
+                int res = DbSqlite.ExecuteScalar(sql_conn, str, com_params);
+                Console.WriteLine(res);
             }
             catch (Exception ex)
             {
