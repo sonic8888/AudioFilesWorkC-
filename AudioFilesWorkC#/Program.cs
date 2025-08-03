14cb2773430f7919f8f016aed6a4d3622bd92bd9
sonic <sonic45600@gmail.com>
2025-08-03 18:12:22 +0300

    исправил GetDataFromYandexDB

--- AudioFilesWorkC#/Program.cs
+++ AudioFilesWorkC#/Program.cs
@@ -19,52 +19,52 @@ namespace AudioFilesWorkC_
 
 
             }
-
-
-            static Track[]? GetDataFromYandexDB()
-            {
-                Track[]? tracks = null;
-                try
-                {
-                    YandexMusic yandexMusic = new();
-                    string? sours_db = YandexMusic.PathDBSqlite;
-                    string sql_conn = DbSqlite.Get_str_connection(sours_db);
-                    List<SqliteParameter> com_params = DbSqlite.Get_list_params(new Dictionary<string, string> { { "value", "5" } });
-                    object? res = DbSqlite.ExecuteScalar(sql_conn, DbSqlite.queries["str1"], com_params);
-
-                    if (res != null)
-                    {
-                        int result = Convert.ToInt32(res);
-                        tracks = new Track[result];
-                        for (int i = 0; i < tracks.Length; i++)
-                        {
-                            tracks[i] = new Track();
-                        }
-                        DbSqlite.ExecuteReader(sql_conn, DbSqlite.queries["str2"], ("TrackId", "GetString"), ref tracks, com_params);
-                        foreach (var item in tracks)
-                        {
-
-                            List<SqliteParameter> lp_title = DbSqlite.Get_list_params(new Dictionary<string, string> { { "value", item.TrackId! } });
-                            DbSqlite.ExecuteReader(sql_conn, DbSqlite.queries["str3"], ("Name", "GetString"), item, lp_title);
-                            DbSqlite.ExecuteReader(sql_conn, DbSqlite.queries["str4"], ("ArtistId", "GetString"), item, lp_title);
-                            List<SqliteParameter> lp_artist = DbSqlite.Get_list_params(new Dictionary<string, string> { { "value", item.ArtistId! } });
-                            DbSqlite.ExecuteReader(sql_conn, DbSqlite.queries["str5"], ("Artist", "GetString"), item, lp_artist);
-
-                        }
-                    }
-
-
-
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                    Environment.Exit(1);
-                }
-                return tracks;
-            }
-
-
         }
+
+        static Track[]? GetDataFromYandexDB()
+        {
+            Track[]? tracks = null;
+            try
+            {
+                YandexMusic yandexMusic = new();
+                string? sours_db = YandexMusic.PathDBSqlite;
+                string sql_conn = DbSqlite.Get_str_connection(sours_db);
+                List<SqliteParameter> com_params = DbSqlite.Get_list_params(new Dictionary<string, string> { { "value", "5" } });
+                object? res = DbSqlite.ExecuteScalar(sql_conn, DbSqlite.queries["str1"], com_params);
+
+                if (res != null)
+                {
+                    int result = Convert.ToInt32(res);
+                    tracks = new Track[result];
+                    for (int i = 0; i < tracks.Length; i++)
+                    {
+                        tracks[i] = new Track();
+                    }
+                    DbSqlite.ExecuteReader(sql_conn, DbSqlite.queries["str2"], ("TrackId", "GetString"), ref tracks, com_params);
+                    foreach (var item in tracks)
+                    {
+
+                        List<SqliteParameter> lp_title = DbSqlite.Get_list_params(new Dictionary<string, string> { { "value", item.TrackId! } });
+                        DbSqlite.ExecuteReader(sql_conn, DbSqlite.queries["str3"], ("Name", "GetString"), item, lp_title);
+                        DbSqlite.ExecuteReader(sql_conn, DbSqlite.queries["str4"], ("ArtistId", "GetString"), item, lp_title);
+                        List<SqliteParameter> lp_artist = DbSqlite.Get_list_params(new Dictionary<string, string> { { "value", item.ArtistId! } });
+                        DbSqlite.ExecuteReader(sql_conn, DbSqlite.queries["str5"], ("Artist", "GetString"), item, lp_artist);
+
+                    }
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Environment.Exit(1);
+            }
+            return tracks;
+        }
+
+
     }
 }
+
